933fa0317f61d657b040111f6ddccb2ae0ef9105
misha <ilayfeeed@gmail.com>
2024-03-01 00:11:00 +0300

    all

--- main.py
+++ main.py
@@ -22,12 +22,12 @@ def get_rooms(req: Request):
 
 @app.get('/history', response_class=HTMLResponse)
 def get_history(req: Request):
-    return templates.TemplateResponse(name='history.html', context={'request': req})
+    return templates.TemplateResponse(name='/history/history.html', context={'request': req})
 
 
 @app.get('/spa', response_class=HTMLResponse)
 def get_spa(req: Request):
-    return templates.TemplateResponse(name='spa.html', context={'request': req})
+    return templates.TemplateResponse(name='/spa/spa.html', context={'request': req})
 
 
 @app.get('/gallery', response_class=HTMLResponse)
@@ -37,10 +37,10 @@ def get_gallery(req: Request):
 
 @app.get('/restraunts', response_class=HTMLResponse)
 def get_restraunts(req: Request):
-    return templates.TemplateResponse(name='restraunts.html', context={'request': req})
+    return templates.TemplateResponse(name='/restraunts/restraunts.html', context={'request': req})
 
 
 if __name__ == '__main__':
     import uvicorn
 
-    uvicorn.run('main:app', host='0.0.0.0', port=80, reload=True, reload_includes=['*.html', '*.css'])
+    uvicorn.run('main:app', host='127.0.0.1', port=80, reload=True, reload_includes=['*.html', '*.css'])
