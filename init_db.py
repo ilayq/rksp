12a1e07609b7654a7dc01bd1a18abb686652b3c9
ilay <asdqwe@gmail.com>
2024-03-22 12:48:22 +0300

    init_db.py

--- init_db.py
+++ init_db.py
@@ -1,10 +1,16 @@
 from api.models.orm import engine, Base
+from sqlite3 import connect
 
 
 async def init():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
+
+    con = connect("db.db")
+    cur = con.cursor()
+    cur.execute("insert into rooms(room_name, num_rooms, num_guests, area, description, room_amount)\
+                values ('lux', 2, 2, 20, 'super room', 2)")
 
 
 if __name__ == '__main__':
